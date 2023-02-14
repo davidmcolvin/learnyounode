09b2b2d2f6b83345babbf615bbdfbf994e0238e9
davidmcolvin <davidmcolvin@gmail.com>
2023-02-13 21:24:55 -0500

    Finish exercise 7 http client intro

--- ex7/http-client.js
+++ ex7/http-client.js
@@ -1,3 +1,30 @@
 const http = require('http');
 
+const url = process.argv[2];
+
+http.get(url, (res) => {
+
+    const {statusCode } = res;
+
+    const contentType = res.headers['content-type'];
+
+    let error;
+
+    if (statusCode !== 200){
+        error = new Error('Request Failed.\n'  +
+                          'Status Code: ${statusCode}');
+    }
+   
+    if (error) {
+        console.error(error.message);
+        // consume response data to free up memory
+        res.resume();
+        return;
+    }
+
+    res.setEncoding('utf8');
+    let rawData = '';
+    res.on('data',(chunk) => {console.log(chunk)});
+
+});
 
